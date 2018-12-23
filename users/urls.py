412b0fd2ed956159031ffabf77c9393c47e7276e
sukubhattu <sukubhattusandesh8@gmail.com>
2018-12-23 17:53:40 +0545

    Basic LogIn system created

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
+from . import views as user_views
+from django.contrib.auth import views as auth_views
 urlpatterns = [
+	path('', user_views.home, name = 'users_index'),
+	path('register/', user_views.register, name = 'user_sign_up'),
+	path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name = 'user_log_in'),
+	path('logout', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name = 'user_log_out'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+	return HttpResponse("hello world")
+
+def register(request):
+	form = UserCreationForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+	return render(request, 'users/register.html', {'form' : form})
+
